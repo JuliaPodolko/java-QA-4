481b5297d6f0d24dfecb5456f8ddf3e423476809
Julia Podolko <Julia Podolkoemail>
2022-06-03 01:12:49 +0500

    end

--- src/main/java/guru/qa/FirstApp.java
+++ src/main/java/guru/qa/FirstApp.java
@@ -11,6 +11,7 @@ public class FirstApp {
         short aShort; // 16 bit, default=0
         int aInt = aByte; // 32 bit, default=0
         long aLong = 123L; // 64 bit, default=0
+        aLong += 1; // aLong = aLong+1
         Integer boxed = 1; // класс-обертка default=null
 
         char[][] array = new char[][] {{'w', 'a', 's'}};
@@ -20,7 +21,54 @@ public class FirstApp {
 
         String aString = "Hello, QA"; // default=null
 
+        // / деление (целочиссленное)
+        // % взятие остатка от деления
+
+        // & и (&&)
+        // | или (||)
+        // ! отрицание
+
+        // != не равно ^
+        // == сравнивает адреса в памяти
+        // для сравнения использовать equals (для строк)
+
+        if (aString != null && aString.equals("Julia")) {
+
+        }
+
+        // instanceof
+        // >= Больше или равно
+        // <= Меньше или равно
+
+
+        aString = 3 > 2 ? "Dima" : "Alex"; // тернарный оператор
+
+        if (3>2) {
+            aString = "Dima";
+        } else {
+            aString = "Alex";
+        }
+
+        switch (aString) {
+            case "Dima":
+                System.out.println("Hi, " + aString);
+                break;
+            case "Alex":
+                System.out.println("Hi, " + aString);
+                break;
+        }
+
+
         getMaxIntNumber();
+        System.out.println(summ(aByte, (int) aLong));
+
+        //Переполнения
+
+        System.out.println(Byte.MAX_VALUE+1);
+        System.out.println(Byte.MIN_VALUE-1);
+
+        System.out.println(Integer.MAX_VALUE + 1);
+        System.out.println(Integer.MIN_VALUE - 1);
 
 
     }
@@ -28,4 +76,9 @@ public class FirstApp {
      //   return Integer.MAX_VALUE;
         System.out.println("max value: " + Integer.MAX_VALUE);
     }
+    static int summ(int first, int second) {
+        return first + second;
+    }
+
+
 }
